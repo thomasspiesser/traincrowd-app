91e88b5ff6f56f94c2eebe1c5183397350dee288
Thomas Spiesser <thomas.spiesser@gmail.com>
2014-10-29 00:10:42 +0100

    updates meteor to 1.0. adds single course view with router data context

--- client/TCClientController.js
+++ client/TCClientController.js
@@ -1,29 +1,13 @@
-Template.navItems.helpers({
-  activeIfTemplateIs: function (template) {
-    var currentRoute = Router.current();
-    return currentRoute &&
-        template === currentRoute.lookupTemplate() ? 'active' : '';
-  }
-});
 
-Template.courseList.helpers({
-  courses: function () {
-    var courses = Courses.find()
-    return courses
-  }
-});
 
 Template.createCourse.events({
   'click #createCourseButton': function (event, template) {
-    console.log('clicked')
-    event.preventDefault();
     var title = template.find("#inputTitleCourse").value;
     var description = template.find("#inputDescriptionCourse").value;
     var maxParticipants = parseInt(template.find("#inputMaxNrParticipantsCourse").value);
     var public = template.find("#publishCourse").checked;
 
     if (title.length && description.length && maxParticipants > 1) {
-      console.log('insert')
       var id = createCourse({
         title: title,
         description: description,
@@ -32,11 +16,12 @@ Template.createCourse.events({
       });
       Session.set("selectedCourse", id);
       Session.set("createError", "");
-      // Router.go("/courseDetail")
+      Router.go("course.show", {_id: id} );
     } else {
       Session.set("createError",
                   "Please, fill out the entire form!");
     }
+    return false
   }
 });
 
