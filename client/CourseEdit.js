49c09a550ae9d6152aba4366a7d7c8fbbd90507a
Thomas Spiesser <thomas.spiesser@gmail.com>
2014-12-30 15:52:11 +0100

    adds datepicker i18n, adds course edit details front end

--- client/CourseEdit.js
+++ client/CourseEdit.js
@@ -1,5 +1,27 @@
 //////////// editCourse template /////////
 
+$.fn.datepicker.dates['de'] = {
+    days: ["Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
+    daysShort: ["Son", "Mon", "Die", "Mit", "Don", "Fre", "Sam", "Son"],
+    daysMin: ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"],
+    months: ["Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"],
+    monthsShort: ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
+    today: "Heute",
+    clear: "Löschen",
+    weekStart: 1,
+    format: "dd.mm.yyyy"
+  };
+
+Template.editCourseDatesServices.rendered=function() {
+    $('#editCourseDates').datepicker({
+      startDate: "-0d",
+      language: "de",
+      todayBtn: true,
+      multidate: 6,
+      todayHighlight: true
+    });
+}
+
 Template.editCourse.events({ 
   'click #editCourseButton': function (event, template) {
     var title = template.find("#inputTitleCourse").value;
