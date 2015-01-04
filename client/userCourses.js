874b98e62c12c0a75a1ec36cec19e46e06ec2a8d
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-01-04 16:59:51 +0100

    adds star-rating-package, shows inquired, current and elapsed

--- client/userCourses.js
+++ client/userCourses.js
@@ -42,11 +42,12 @@ Template.userCourses.helpers({
       return false
   },
   elapsedCourses: function () {
-    var elapsed = Elapsed.find( { "elapsed.participants": Meteor.userId() }, {fields: {course:1, courseDate:1}} ).fetch();
+    var elapsed = Elapsed.find( { participants: Meteor.userId() }, {fields: {course:1, courseDate:1}} ).fetch();
     if (elapsed.length) {
       for (var i = 0; i < elapsed.length; i++) {
         var courseId = elapsed[i].course
-        var course = Courses.findOne( { _id: courseId }, {fields: {_id:0, logo:1, title:1, rating:1}} )
+        var course = Courses.findOne( { _id: courseId }, {fields: {logo:1, title:1, rating:1}} )
+        delete course._id;
         _.extend(elapsed[i],course);
       }
       return elapsed;
@@ -55,12 +56,14 @@ Template.userCourses.helpers({
       return false
   },
   myRating: function (id) {
-    var elapsed = Elapsed.findOne( {_id: id }, {fields: {_id:0, myRating:1}} );
-
-    var myRating = _.find(elapsed, function (item) { 
-        return item.userId === Meteor.userId();
+    var elapsed = Elapsed.findOne( {_id: id }, {fields: {ratings:1}} );
+    var myRating = _.find(elapsed.ratings, function (item) {
+      return item.participant === Meteor.userId();
       });
     console.log(myRating)
-    return myRating
+    if (typeof myRating !== 'undefined')
+      return (myRating.rating).toString() // toString coz if 0 then = false
+    else
+      return false
   }
 });
