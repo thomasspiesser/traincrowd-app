75aca9a26ad26bb3fdb44095a2a54a314ab55a9d
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-01-04 15:08:41 +0100

    adds userCourses logic and display hosted, inquired, current and elapsed in tables on userCourses page

--- client/userCourses.js
+++ client/userCourses.js
@@ -11,5 +11,56 @@ Template.userCourses.helpers({
   },
   log: function(){
     console.log(this);
+  },
+  hostedCourses: function () {
+    return Courses.find( { owner: Meteor.userId() }, {fields: {logo:1, title:1, rating:1}} )
+  },
+  inquiredCourses: function () {
+    var inquired = Inquired.find( { inquirer: Meteor.userId() }, {fields: {course:1, inquiredDates:1}} ).fetch();
+    if (inquired.length) {
+      for (var i = 0; i < inquired.length; i++) {
+        var courseId = inquired[i].course
+        var course = Courses.findOne( { _id: courseId }, {fields: {_id:0, logo:1, title:1, rating:1}} )
+        _.extend(inquired[i],course);
+      }
+      return inquired;
+    }
+    else
+      return false
+  },
+  currentCourses: function () {
+    var current = Current.find( { "current.participants": Meteor.userId() }, {fields: {course:1, courseDate:1}} ).fetch();
+    if (current.length) {
+      for (var i = 0; i < current.length; i++) {
+        var courseId = current[i].course
+        var course = Courses.findOne( { _id: courseId }, {fields: {_id:0, logo:1, title:1, rating:1}} )
+        _.extend(current[i],course);
+      }
+      return current;
+    }
+    else
+      return false
+  },
+  elapsedCourses: function () {
+    var elapsed = Elapsed.find( { "elapsed.participants": Meteor.userId() }, {fields: {course:1, courseDate:1}} ).fetch();
+    if (elapsed.length) {
+      for (var i = 0; i < elapsed.length; i++) {
+        var courseId = elapsed[i].course
+        var course = Courses.findOne( { _id: courseId }, {fields: {_id:0, logo:1, title:1, rating:1}} )
+        _.extend(elapsed[i],course);
+      }
+      return elapsed;
+    }
+    else
+      return false
+  },
+  myRating: function (id) {
+    var elapsed = Elapsed.findOne( {_id: id }, {fields: {_id:0, myRating:1}} );
+
+    var myRating = _.find(elapsed, function (item) { 
+        return item.userId === Meteor.userId();
+      });
+    console.log(myRating)
+    return myRating
   }
 });
