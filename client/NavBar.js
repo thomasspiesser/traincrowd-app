5401e22f362dfcb388a971f740734e88c2c78467
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-01-08 23:40:21 +0100

    nicer course display, adds link to admin if isMember

--- client/NavBar.js
+++ client/NavBar.js
@@ -1,3 +1,12 @@
+Template.navItemsLeft.helpers({
+	isMember: function () {
+		if (Meteor.userId()) {
+			if (Meteor.user().emails[0].address === 'thomas@traincrowd.de')
+				return true
+		}
+	}
+});
+
 Template.navItemsRight.events({
 	'click #logout': function () {
 		Meteor.logout();
