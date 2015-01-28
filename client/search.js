00c339a24374de166dd24bbf5bb8a922ddd98331
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-01-28 13:53:41 +0100

    hides filter for categories in search

--- client/search.js
+++ client/search.js
@@ -1,13 +1,13 @@
-Template.filters.events({
-  'click .filter' : function (event, template) {
-    var category = event.target.id;
-    var instance = EasySearch.getComponentInstance(
-      { index : 'courses' }
-    );
+// Template.filters.events({
+//   'click .filter' : function (event, template) {
+//     var category = event.target.id;
+//     var instance = EasySearch.getComponentInstance(
+//       { index : 'courses' }
+//     );
 
-    // Change the currently filteredCategories like this
-    EasySearch.changeProperty('courses', 'filteredCategory', category);
-    // Trigger the search again, to reload the new products
-    instance.triggerSearch();
-  }
-});
+//     // Change the currently filteredCategories like this
+//     EasySearch.changeProperty('courses', 'filteredCategory', category);
+//     // Trigger the search again, to reload the new products
+//     instance.triggerSearch();
+//   }
+// });
