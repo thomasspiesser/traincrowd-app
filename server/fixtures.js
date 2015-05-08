fa8ebf53d1caf30ba95ff096f6b5c97bd8082266
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-05-08 13:36:17 +0200

    removes unused packages and gatekeeper and old fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,7 @@
-Meteor.startup(function () {
-	Courses.find( {slug: {$exists: false}}, {fields: {_id:1, title:1}} ).forEach( function (course) {
-		var slug = slugify(course.title);
-		Courses.update( { _id: course._id }, {$set: {slug: slug}} );
-	} );
+// Meteor.startup(function () {
+// 	Courses.find( {slug: {$exists: false}}, {fields: {_id:1, title:1}} ).forEach( function (course) {
+// 		var slug = slugify(course.title);
+// 		Courses.update( { _id: course._id }, {$set: {slug: slug}} );
+// 	} );
 
-});
+// });
