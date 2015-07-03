a4e9a2e045e5ec48dacfc62bb5ed20244256f7da
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-07-03 14:41:03 +0200

    adds custom createUser method on server and client side logic to call and login, adds title and name to user registration

--- client/courses/courseBooking/bookCourseRegister.js
+++ client/courses/courseBooking/bookCourseRegister.js
@@ -1,3 +1,5 @@
+var self;
+
 Template.bookCourseRegister.events({
   'click #book-course-login-button': function (event, template) {
     var email = template.find('#sign-in-email').value;
@@ -13,10 +15,10 @@ Template.bookCourseRegister.events({
       return false;
     }
 
-    var self = this;
+    self = this;
 
     Meteor.loginWithPassword(email, password, function(error){
-      if (error)
+      if ( error )
         toastr.error( error.reason );
       else {
         if ( ! self.customer ){
@@ -26,7 +28,7 @@ Template.bookCourseRegister.events({
             argValue: ''
           };
           Meteor.call('updateBooking', args, function (error, result) {
-            if (error)
+            if ( error )
               toastr.error( error.reason );
           });
         }
@@ -38,24 +40,91 @@ Template.bookCourseRegister.events({
     });
   },
   'click #book-course-register-button': function (event, template) {
-    var email = template.find('#sign-in-email').value;
-    var password = template.find('#sign-in-password').value;
+    var email = template.find('#sign-up-email').value;
+    var emailAgain = template.find('#sign-up-email-again').value;
+    var password = template.find('#sign-up-password').value;
+    var passwordAgain = template.find('#sign-up-password-again').value;
+    var title = template.find('#sign-up-title').value;
+    var name = template.find('#sign-up-name').value;
 
-    Accounts.createUser({email: email, password : password}, function(err){
-      if (err) {
-        Session.set( "createError", "Sorry, "+err.reason );
-      } else {
-        Session.set( "createError", '' );
-        $('#loginModal').modal('hide');
+    $('.form-control').parent().removeClass('has-error');
+    $('.help-block').text('');
+
+    if ( ! name.length ) {
+      formFeedbackError( '#sign-up-name', '#help-text-sign-up-name', 'Bitte geben Sie Ihren Namen an.', "Der Name darf nicht leer sein." );
+      return false;
+    }
+
+    if ( ! EMAIL_REGEX.test( email ) ) {
+      formFeedbackError( '#sign-up-email', '#help-text-sign-up-email', 'Bitte geben Sie eine valide Email-Adresse an.', "Das ist keine valide Email." );
+      return false;
+    }
+
+    if ( ! EMAIL_REGEX.test( emailAgain ) ) {
+      formFeedbackError( '#sign-up-email-again', '#help-text-sign-up-email-again', 'Bitte geben Sie eine valide Email-Adresse an.', "Das ist keine valide Email." );
+      return false;
+    }
+
+    if ( email !== emailAgain ) {
+      formFeedbackError( '#sign-up-email', '#help-text-sign-up-email', 'Bitte überprüfen Sie, ob Sie tatsächlich zweimal die gleiche Email eingegeben haben.', "Bitte überprüfen Sie, ob Sie tatsächlich zweimal die gleiche Email eingegeben haben." );
+      return false;
+    }
+
+    if ( ! password.length ) {
+      formFeedbackError( '#sign-up-password', '#help-text-sign-up-password', 'Bitte geben Sie ein Passwort an.', "Das Passwort darf nicht leer sein." );
+      $('#sign-up-password').val('');
+      $('#sign-up-password-again').val('');
+      return false;
+    }
+
+    if ( password !== passwordAgain ) {
+      formFeedbackError( '#sign-up-password', '#help-text-sign-up-password', 'Bitte überprüfen Sie, ob Sie tatsächlich zweimal das gleiche Passwort eingegeben haben.', "Bitte überprüfen Sie, ob Sie tatsächlich zweimal das gleiche Passwort eingegeben haben." );
+      $('#sign-up-password').val('');
+      $('#sign-up-password-again').val('');
+      return false;
+    }
+
+    self = this;
+
+    var profile = {
+      title: title,
+      name: name
+    };
+
+    var hash = Accounts._hashPassword(password);
+
+    var options = {
+      email: email,
+      password: hash,
+      profile: profile
+    };
+
+    Meteor.call('createUserServer', options, function (error, result) {
+      if ( error )
+        toastr.error( error.reason );
+      if ( result ) {
+        Meteor.loginWithPassword(email, password, function( error ){
+          if (error)
+            toastr.error( error.reason );
+          else {
+            if ( ! self.customer ){
+              var args = {
+                bookingId: self._id,
+                argName: 'customer',
+                argValue: ''
+              };
+              Meteor.call('updateBooking', args, function (error, result) {
+                if ( error )
+                  toastr.error( error.reason );
+              });
+            }
+            Session.set('bookCourseTemplate', "bookCourseAddress");
+
+            $('#bookCourseRegister').children('.progress-tracker').removeClass('active').addClass('inactive');
+            $('#bookCourseAddress').children('.progress-tracker').removeClass('inactive').addClass('active');
+          }
+        });
       }
     });
-
-    Session.set('bookCourseTemplate', "bookCourseAddress");
-
-    $('#bookCourseRegister').children('.progress-tracker').removeClass('active').addClass('inactive');
-    $('#bookCourseAddress').children('.progress-tracker').removeClass('inactive').addClass('active');
-
-    return false;
   }
 });
-
---
+++ server/accounts.js
@@ -0,0 +1,30 @@
+Meteor.methods({
+	createUserServer: function ( options ) {
+		if ( this.userId )
+			throw new Meteor.Error(444, "Sie sind bereits eingelogged.");
+
+		check(options, {
+			email: String,
+			password: {
+				digest: String,
+				algorithm: String
+			},
+			profile: Match.Optional( Object )
+		});
+
+		if (! EMAIL_REGEX.test( options.email ))
+      throw new Meteor.Error(403, "Bitte überprüfen Sie, ob Sie eine echte Email Adresse eingegeben haben.");
+
+    // create user
+		var userId = Accounts.createUser( { email: options.email, password : options.password, profile: options.profile } );
+		// safety belt. createUser is supposed to throw on error. send 500 error
+    // instead of sending a verification email with empty userid.
+		if ( ! userId )
+			throw new Meteor.Error(444, "Beim Erstellung des Nutzerkontos ist ein Fehler aufgetreten.");
+		else 
+			Accounts.sendVerificationEmail( userId, options.email );
+		
+		return userId;                
+
+	}
+});
