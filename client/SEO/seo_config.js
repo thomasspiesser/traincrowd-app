4a44e58be7e9449d0c81edfa9cf7ffa751308d42
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-07-10 11:33:49 +0200

    adds seo package for meta tags

---
+++ client/SEO/seo_config.js
@@ -0,0 +1,15 @@
+Meteor.startup(function() {
+  return SEO.config({
+    title: 'traincrowd - improve yourself. learn together',
+    meta: {
+      'description': "traincrowd ist die innovative crowdsourcing-Plattform für Trainings, Seminare und Weiterbildungen. Auf traincrowd finden Sie Berlins Top-Auswahl, deshalb ist bei uns für Sie garantiert immer der richtige Kurs dabei."
+    },
+    og: {
+      'image': '/images/Landing-top.jpeg' 
+   },
+   ignore: {
+    meta: ['google-site-verification', 'fb:app_id', 'fragment', 'viewport'],
+    link: ['stylesheet', 'icon', 'apple-touch-icon']
+   }
+  });
+});
