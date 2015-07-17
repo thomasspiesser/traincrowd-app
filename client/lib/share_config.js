48e985531b24da6998a938c3d08ff94e2ed83e3e
Edgard Berendsen <eberendsen@ebm.cl>
2015-07-17 16:23:56 +0200

    removed the default classes for the share buttons

--- client/lib/share_config.js
+++ client/lib/share_config.js
@@ -14,11 +14,11 @@ Share.configure({
       // 'pinterest': {}
   },
   // siteOrder: [ 'twitter', 'facebook', 'instagram', 'googleplus', 'pinterest'], // careful, must name all of them in here, coz overrides
-  classes: "btn btn-xs", // string (default: 'large btn')
+  classes: "", // string (default: 'large btn')
                         // The classes that will be placed on the sharing buttons, bootstrap by default.
   iconOnly: true,      // boolean (default: false)
                         // Don't put text on the sharing buttons
-  applyColors: true,     // boolean (default: true)
+  applyColors: false,     // boolean (default: true)
                         // apply classes to inherit each social networks background color
   faSize: '',            // font awesome size
   faClass: ''       // font awesome classes like square
