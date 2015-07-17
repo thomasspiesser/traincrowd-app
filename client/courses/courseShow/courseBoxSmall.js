fb23cecad59e4f23ca7babd95c5841391396b5b8
Edgard Berendsen <eberendsen@ebm.cl>
2015-07-17 16:23:29 +0200

    added the share container div and buttons, added the share button toggle

--- client/courses/courseShow/courseBoxSmall.js
+++ client/courses/courseShow/courseBoxSmall.js
@@ -2,4 +2,7 @@ Template.courseBoxSmall.events({
   'click .btn-bar': function (event, template) {
     $(event.currentTarget).toggleClass('in');
   },
+  'click .share-open': function (event, template) {
+    $(event.currentTarget).next().toggleClass('in');
+  },
 });
