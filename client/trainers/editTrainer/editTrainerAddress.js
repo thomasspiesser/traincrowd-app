1eff43cab78cb7a892e34cab4a340f8f691c3565
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-08-05 17:41:01 +0200

    bug fix edit user/trainer address

--- client/trainers/editTrainer/editTrainerAddress.js
+++ client/trainers/editTrainer/editTrainerAddress.js
@@ -6,7 +6,7 @@ Template.editTrainerAddress.events({
     lazysaveUserField( { argName: field, argValue: event.currentTarget.value } );
   },
   'click #saveEditTrainerAddress': function (event, template) {
-    if (! this.profile.street || ! this.profile.streetNumber || ! this.profile.plz || ! this.profile.city) {
+    if (! this.profile.billingAddresses[0].street || ! this.profile.billingAddresses[0].streetNumber || ! this.profile.billingAddresses[0].plz || ! this.profile.billingAddresses[0].city) {
       $('.form-group').addClass('has-error');
       $('#help-text-edit-street').text('Bitte geben Sie eine vollständige Rechnungsadresse an.').fadeIn(300);
       toastr.error( "Bitte machen Sie vollständige Angaben zu Ihrer Adresse." );
