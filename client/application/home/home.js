76128aceb273d06ed44353832564ccceb7ff87c0
Edgard Berendsen <eberendsen@ebm.cl>
2015-08-05 17:45:57 +0200

    added ... to "Suchen"

--- client/application/home/home.js
+++ client/application/home/home.js
@@ -14,7 +14,7 @@ Template.home.onRendered(function() {
   var originalPlaceholder = ($('#home-search-form').find("input[type=text]").attr("placeholder"));
   function checkWidth() {
     if ($(window).width() < 992) {
-      $('#home-search-form').find("input[type=text]").attr("placeholder","Suchen");
+      $('#home-search-form').find("input[type=text]").attr("placeholder","Suchen...");
     } else {
       $('#home-search-form').find("input[type=text]").attr("placeholder",originalPlaceholder);
     }
