901de9ffc76c0549a12036a6a9f2045fe005aa12
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-08-07 10:01:12 +0200

    remove code for Inquired

--- client/courses/courseBooking/bookCourse.js
+++ client/courses/courseBooking/bookCourse.js
@@ -38,50 +38,6 @@ Template.bookCourse.events({ 
   }
 });
 
-
-//////////// courseInquiry template /////////
-
-// Template.courseInquiry.rendered=function() {
-//     $('#inquireDatesDatepicker').datepicker({
-//       format: "dd/mm/yyyy",
-//       weekStart: 1,
-//       language: "de",
-//       todayBtn: true,
-//       multidate: true,
-//       todayHighlight: true
-//     });
-// }
-
-// Template.courseInquiry.events({
-//   'click #cancelBookCourseButton': function () {
-//     Router.go("course.show", {_id: this._id} );
-//   },
-//   'click #inquireCourseDatesButton': function (event, template) {
-//     var inquiredDates = template.find("#inquireDatesDatepicker").value;
-//     if (inquiredDates.length > 1) {
-//       var courseId = this._id;
-//       var inquiredDatesArray = inquiredDates.split(",");
-//       var options = {
-//         courseId: courseId,
-//         courseOwner: this.owner,
-//         dates: inquiredDatesArray
-//       }
-//       Meteor.call('createInquired', options, function (error, result) {
-//         if(error) {
-//           toastr.error(error.reason);
-//         } else {
-//           // Session.set( "instanceId", result );
-//           Router.go("course.show", {_id: courseId} );
-//           toastr.success('Successfully submitted dates to trainer.');
-//         }
-//       });
-//     } else {
-//       toastr.error('Please, choose at least one date!');
-//     }
-//     return false
-//   }
-// }); 
-
 //////////// paymentModal template /////////
 
 Template.paymentModal.helpers({
@@ -91,15 +47,6 @@ Template.paymentModal.helpers({
       return current.participants.length === this.maxParticipants;
     }
     return false;
-  },
-  countdown: function () {
-    return "bitte warten: " + Session.get('time');
-  },
-  message: function () {
-    return Session.get("bookingMessage");
-  },
-  errorMessage: function () {
-    return Session.get("errorMessage");
   },
   formatedDates: function () {
     var courseDate = Session.get("currentDate");
@@ -113,43 +60,3 @@ Template.paymentModal.helpers({
     return ( ( this.fee + commision ) / this.maxParticipants ).toFixed(2);
   }
 });  
-
-Template.paymentModal.events({
-  'click #bookCourseButton': function (event, template) {
-    // data context is still course: access using 'this.'
-    var currentId = Session.get("currentId");
-
-    Session.set("bookingMessage",
-                  "Simulation der Bezahlfunktion, warten auf den Callback des Bezahlanbieters!");
-    var clock = 5;
-    timeLeft = function(){
-      if (clock > 0) {
-        clock--;
-        Session.set('time',clock);
-      } else {
-        Session.set('time','');
-        Session.set("bookingMessage",'');
-        Meteor.clearInterval(interval);
-        Session.set('errorMessage', '');
-        $('#paymentModal').modal('hide');
-        // assume success, push userId into current.participants 
-        Meteor.call('addParticipant', currentId, function (error, result) {
-          if (error)
-            toastr.error(error.reason);
-          else {
-            toastr.success('Buchung erfolgreich.');
-            // toastr.success('Bitte vervollständigen Sie jetzt Ihr Profil und teilen Sie uns Ihre Erwartungen mit. So kann sich ihr Trainer optimal auf Sie vorbereiten.');
-            Meteor.setTimeout( redirect , 3000 );
-          }
-        });
-      }
-    };
-    redirect = function( ) {
-      Router.go('edit.user', {_id: Meteor.userId()});
-    };
-    var interval = Meteor.setInterval(timeLeft, 1000);
-  },
-  'click #modalCloseButton': function () {
-    Session.set('errorMessage', '');
-  }
-});
--- server/serverMethods.js
+++ server/serverMethods.js
@@ -1,31 +1,4 @@
 Meteor.methods({
-	// createInquired: function (options) {
- //    if (! this.userId)
- //      throw new Meteor.Error(403, "Sie müssen eingelogged sein!");
- //    var id = Inquired.insert({
- //      owner: options.owner,
- //      course: options.course,
- //      inquirer: this.userId,
- //      inquiredDates: options.dates,
- //      createdAt: new Date()
- //    });
- //    return id;
- //    // TODO: send email to owner
- //  },
- //  confirmInquired: function (options) {
- //    // TODO: check that user._id = course.owner
- //    // date confirmed so insert into current
- //    Current.insert({
- //      _id: options.id,
- //      course: options.course,
- //    	owner: options.owner,
- //      participants: [options.inquirer],
- //      courseDate: options.confirmedDate
- //    });
-    
- //    //remove from Inquired:
- //    Inquired.remove({_id: options.id});
- //  },
   createEvent: function (options) {
     check(options, {
       courseId: NonEmptyString,
@@ -203,7 +176,6 @@ Meteor.methods({
 Houston.add_collection(Meteor.users);
 Houston.add_collection(Houston._admins);
 Houston.add_collection(Courses);
-Houston.add_collection(Inquired);
 Houston.add_collection(Current);
 Houston.add_collection(Elapsed);
 Houston.add_collection(Categories);
