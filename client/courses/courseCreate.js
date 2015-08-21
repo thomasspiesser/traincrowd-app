fcfe8f74653250b9c9869b14639e1163ea7bde1e
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-08-21 16:10:08 +0200

    styles create.course

--- client/courses/courseCreate.js
+++ client/courses/courseCreate.js
@@ -1,23 +1,25 @@
 Template.createCourse.events({
-	'click #createCourseButton': function (event, template) {
+	'click #createCourseButton': function ( event, template ) {
 		event.preventDefault();
     var title = template.find("#inputTitleCourse").value;
-    if (title.length > 120) {
+    if ( title.length > 120 ) {
       toastr.error( "Der Titel darf nicht mehr als 120 Zeichen haben." );
       return false;
     }
 
-    if (title.length) {
-      Meteor.call('createCourse', title, function (error, slug) {
-      	if (error) {
+    if ( title.length ) {
+      Meteor.call( 'createCourse', title, function ( error, slug ) {
+      	if ( error ) {
       		toastr.error( error.reason );
-      	} else {
+      	}
+        else {
       		toastr.success('Wir bitten nun noch um ein paar zusätzliche Infos.' );
-      		Router.go("course.edit", {slug: slug} );
+      		Router.go("course.edit", { slug: slug } );
       	}
       });  
-    } else {
-      toastr.error( "Bitte gib deinem Kurs einen Titel!" );
+    }
+    else {
+      toastr.error( "Bitte geben Sie Ihrem Kurs einen Titel!" );
     }
     return false;
   }
