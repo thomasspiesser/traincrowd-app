85a58c40160fabaa39b3ca9d90507a2467d9becd
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-08-26 11:30:37 +0200

    adds logic for booking.hasShared to true on sharing window close, no way to see here if it actually happend :(

--- client/courses/courseBooking/bookCourseThankYou.js
+++ client/courses/courseBooking/bookCourseThankYou.js
@@ -10,10 +10,36 @@ Template.bookCourseThankYou.helpers({
 	shareData: function () {
 		var course = Courses.findOne( { _id : this.course }, { fields: { title: 1, description: 1, slug: 1 } } );
     var data = {
-      title: course.title,
-      description: course.description,
-      url: Meteor.absoluteUrl() + 'course/' + course.slug
+      title: encodeURIComponent( course.title ),
+      description: encodeURIComponent( course.description ),
+      url: encodeURI( Meteor.absoluteUrl() + 'course/' + course.slug )
     };
     return data;
+  }
+});
+
+Template.bookCourseThankYou.events({
+  'click .btn-social': function ( event ) {
+    event.preventDefault();
+    var url = event.currentTarget.href;
+    var windowName = '_blank';
+    var windowSizeX = '600';
+    var windowSizeY = '460';
+    var windowSize = 'width=' + windowSizeX + ',height=' + windowSizeY;
+
+    var sharewindow = window.open( url, windowName, windowSize );
+    var timer = Meteor.setInterval( checkClosed, 500 );
+    var bookingId = this._id;
+    console.log(bookingId);
+
+    function checkClosed() {
+      if ( sharewindow.closed ) {
+        Meteor.clearInterval( timer );
+        Meteor.call('setHasShared', bookingId, function ( error, result ) {
+          if ( error )
+            toastr.error( error.reason );
+        });
+      }
+    }
   }
 });
