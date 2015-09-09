d9eb31fc043fe3edd7e0d92bbd0a2ce14b67b313
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-09-09 14:20:20 +0200

    inits kadira only in production env

--- server/kadira.js
+++ server/kadira.js
@@ -1,1 +1,2 @@
-Kadira.connect('gQaPLeCcRo2FrpQ79', '61658c27-8013-43c6-8e6f-88d398509576')
+if ( process.env.NODE_ENV === 'production' )
+	Kadira.connect('gQaPLeCcRo2FrpQ79', '61658c27-8013-43c6-8e6f-88d398509576');
