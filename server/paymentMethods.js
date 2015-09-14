9df6c88153a90fe7b164a9413afac415bce37c7c
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-09-14 11:45:46 +0200

    fixes too long description paymill transaction

--- server/paymentMethods.js
+++ server/paymentMethods.js
@@ -51,7 +51,7 @@ Meteor.methods({
           amount      : options.amount,
           currency    : 'EUR',
           token       : options.token,
-          description : 'user: ' + this.userId + ' ' + displayName( user ) + '; course: ' + courseId + '; event: ' + currentId
+          description : 'user: ' + this.userId + ' ' + displayName( user ) + '; booking: ' + booking._id
         });
 
         // update booking
