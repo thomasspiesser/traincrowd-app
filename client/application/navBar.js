4d354f1c3f9dfdc218009d2406b9eed92160cd3c
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-09-24 13:08:11 +0200

    adds i18n package, startup lang detection and persistent storage on lang selection

--- client/application/navBar.js
+++ client/application/navBar.js
@@ -1,10 +1,23 @@
+Template.navItemsLeft.helpers({
+  langCode: function () {
+    return i18n.getLanguage();
+  }
+});
+
+Template.navItemsLeft.events({
+  'click .langSelector': function (event) {
+    i18n.setLanguage(event.currentTarget.text);
+    Session.setPersistent( 'Language', event.currentTarget.text );
+  }
+});
+
 Template.navItemsRight.events({
-	'click #logout': function () {
-		Meteor.logout( function (error) {
-			if ( error )
-				toastr.error( error.reason );
-			else
-				Router.go('home');
-		});
-	}
+  'click #logout': function () {
+    Meteor.logout( function (error) {
+      if ( error )
+        toastr.error( error.reason );
+      else
+        Router.go('home');
+    });
+  }
 });
