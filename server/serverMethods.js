9fe5291e2f30970bedd3a42f48c9016e93373a77
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-09-30 09:48:24 +0200

    replaces yogiben with houston:admin again, for the time being, since yogiben is broke with meteor 1.2

--- server/serverMethods.js
+++ server/serverMethods.js
@@ -173,42 +173,43 @@ Meteor.methods({
 
 //// custom admin methods
 
-// Houston.add_collection(Meteor.users);
-// Houston.add_collection(Houston._admins);
-// Houston.add_collection(Courses);
-// Houston.add_collection(Current);
-// Houston.add_collection(Elapsed);
-// Houston.add_collection(Categories);
+Houston.add_collection(Meteor.users);
+Houston.add_collection(Houston._admins);
+Houston.add_collection(Courses);
+Houston.add_collection(Current);
+Houston.add_collection(Elapsed);
+Houston.add_collection(Bookings);
+Houston.add_collection(Categories);
 
-// Houston.methods( Courses, {
-//   Publish: function ( course ) {
-//     Courses.update(course._id, { $set: { isPublic: true, hasPublishRequest: false } }, { validate: false } );
-//     return "Der Kurs: '"+ course.title + "' ist jetzt online!";
-//   },
-//   Unpublish: function ( course ) {
-//   	Courses.update(course._id, { $set: { isPublic: false } }, { validate: false } );
-//     return "Ok, der Kurs: '"+ course.title + "' ist offline.";
-//   }
-// });
+Houston.methods( Courses, {
+  Publish: function ( course ) {
+    Courses.update(course._id, { $set: { isPublic: true, hasPublishRequest: false } }, { validate: false } );
+    return "Der Kurs: '"+ course.title + "' ist jetzt online!";
+  },
+  Unpublish: function ( course ) {
+  	Courses.update(course._id, { $set: { isPublic: false } }, { validate: false } );
+    return "Ok, der Kurs: '"+ course.title + "' ist offline.";
+  }
+});
 
-// Houston.methods( Meteor.users, {
-//   Publish: function ( user ) {
-//     Meteor.users.update(user._id, { $set: { isPublic: true, hasPublishRequest: false } }, { validate: false } );
-//     return "Das Profil von: '"+ user.profile.name + "' ist jetzt online!";
-//   },
-//   Unpublish: function ( user ) {
-//     Meteor.users.update(user._id, { $set: { isPublic: false } }, { validate: false } );
-//     return "Ok, das Profil von: '"+ user.profile.name + "' ist offline.";
-//   }
-// });
+Houston.methods( Meteor.users, {
+  Publish: function ( user ) {
+    Meteor.users.update(user._id, { $set: { isPublic: true, hasPublishRequest: false } }, { validate: false } );
+    return "Das Profil von: '"+ user.profile.name + "' ist jetzt online!";
+  },
+  Unpublish: function ( user ) {
+    Meteor.users.update(user._id, { $set: { isPublic: false } }, { validate: false } );
+    return "Ok, das Profil von: '"+ user.profile.name + "' ist offline.";
+  }
+});
 
-// Houston.methods( Current, {
-//   Confirm: function ( current ) {
-//     Current.update( current._id, { $set: { confirmed: true } }, { validate: false } );
-//     return "Das Event am " + current.courseDate + " zum Kurs: '"+ current.title + "' findet statt!";
-//   },
-//   Unconfirm: function ( current ) {
-//     Current.update( current._id, { $set: { confirmed: false } }, { validate: false } );
-//     return "Ok, das Event am " + current.courseDate + " zum Kurs: '"+ current.title + "' findet nicht statt.";
-//   }
-// });
+Houston.methods( Current, {
+  Confirm: function ( current ) {
+    Current.update( current._id, { $set: { confirmed: true } }, { validate: false } );
+    return "Das Event am " + current.courseDate + " zum Kurs: '"+ current.title + "' findet statt!";
+  },
+  Unconfirm: function ( current ) {
+    Current.update( current._id, { $set: { confirmed: false } }, { validate: false } );
+    return "Ok, das Event am " + current.courseDate + " zum Kurs: '"+ current.title + "' findet nicht statt.";
+  }
+});
