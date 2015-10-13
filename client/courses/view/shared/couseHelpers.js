0a8d44bb8338fed99448cb2e52e3d25eeaa6e984
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-13 11:34:46 +0200

    adds getImage course helper

--- client/courses/view/shared/couseHelpers.js
+++ client/courses/view/shared/couseHelpers.js
@@ -8,12 +8,6 @@ courseHelpers = {
   percentFull: function ( course ) {
     return ( this.participants.length / course.maxParticipants ).toFixed(1) * 100;
   },
-  trainerImageId: function (id) {
-    var trainer = Meteor.users.findOne( {_id: id}, {fields: {"profile.imageId": 1}} );
-    if (trainer.profile && trainer.profile.imageId)
-      return trainer.profile.imageId;
-    return false;
-  },
   openSpots: function ( course ) {
     return course.maxParticipants - this.participants.length;
   },
