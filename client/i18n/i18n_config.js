139e76369a31d9cb6d79ddce68af9f19d6571d85
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-14 10:11:32 +0200

    fix browser language selection bug

--- client/i18n/i18n_config.js
+++ client/i18n/i18n_config.js
@@ -3,8 +3,11 @@ Meteor.startup(function() {
   if ( Session.get( 'Language' ) )
     lang = Session.get( 'Language' );
   else {
-    // detect the language used by the browser
+    // detect the language used by the browser, e.g. en-us or de-de
     lang = window.navigator.userLanguage || window.navigator.language;
+    lang = lang.split('-')[0];
+    if ( lang !== 'de' && lang !== 'en' ) 
+      lang = 'de'; // our default in case we don't have tansl for this language
   }
   i18n.setLanguage( lang );
 });
