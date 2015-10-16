89ed64f72d7986b1110abed2fd79f0481ab3258d
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-16 10:09:04 +0200

    removes validation for the time being before publish request and on singleCourse/UserField-update coz is broken - to be fixed!!!

--- client/courses/edit/editCoursePreview.js
+++ client/courses/edit/editCoursePreview.js
@@ -1,17 +1,17 @@
 Template.editCoursePreview.events({
   'click #requestPublicCourseButton': function () {
     if (confirm( "Möchten Sie Ihren Kurs jetzt freischalten lassen? Drücken Sie ok und wir prüfen Ihre Angaben. Wir lassen Sie wissen, sobald wir Ihren Kurs freigeschaltet haben." ) ) {
-      var course = Courses.findOne({_id: this._id}, {fields:{_id:0, updatedAt:0}});
-      var isValid = courseSchema.namedContext().validate(course);
-      if (!isValid) {
-        var invalidKeys = courseSchema.namedContext().invalidKeys();
-        for (i=0; i<invalidKeys.length; i++) {
-          var errTitle = invalidKeys[i].name,
-              errMsg = courseSchema.namedContext().keyErrorMessage( errTitle );
-          toastr.error( errMsg );
-        }
-        return false;
-      }
+      // var course = Courses.findOne( { _id: this._id }, { fields: { updatedAt:0 } } );
+      // var isValid = courseSchema.namedContext().validate( _.omit( course, '_id') );
+      // if (!isValid) {
+      //   var invalidKeys = courseSchema.namedContext().invalidKeys();
+      //   for (i=0; i<invalidKeys.length; i++) {
+      //     var errTitle = invalidKeys[i].name,
+      //         errMsg = courseSchema.namedContext().keyErrorMessage( errTitle );
+      //     toastr.error( errMsg );
+      //   }
+      //   return false;
+      // }
       var options = {
         what: 'Kurs',
         itemId: this._id,
--- client/trainers/edit/editTrainerPreview.js
+++ client/trainers/edit/editTrainerPreview.js
@@ -1,17 +1,17 @@
 Template.editTrainerPreview.events({
   'click #requestPublicProfileButton': function () {
     if (confirm( "Möchten Sie Ihr Profil jetzt freischalten lassen? Drücken Sie ok und wir prüfen Ihre Angaben. Wir lassen Sie wissen, sobald Ihr Profil freigeschaltet ist." ) ) {
-      var user = Meteor.users.findOne({_id: this._id}, {fields: { _id: 0, updatedAt:0 } } );
-      var isValid = userSchema.namedContext().validate(user);
-      if (!isValid) {
-        var invalidKeys = userSchema.namedContext().invalidKeys();
-        for (i=0; i<invalidKeys.length; i++) {
-          var errTitle = invalidKeys[i].name,
-              errMsg = userSchema.namedContext().keyErrorMessage( errTitle );
-          toastr.error( errMsg );
-        }
-        return false;
-      }
+      // var user = Meteor.users.findOne({_id: this._id}, {fields: { updatedAt:0 } } );
+      // var isValid = userSchema.namedContext().validate( _.omit( user, '_id') );
+      // if (!isValid) {
+      //   var invalidKeys = userSchema.namedContext().invalidKeys();
+      //   for (i=0; i<invalidKeys.length; i++) {
+      //     var errTitle = invalidKeys[i].name,
+      //         errMsg = userSchema.namedContext().keyErrorMessage( errTitle );
+      //     toastr.error( errMsg );
+      //   }
+      //   return false;
+      // }
       Meteor.call( 'setProfilePublishRequest', function ( error, result ) {
         if ( error ) 
           toastr.error( error.reason );
