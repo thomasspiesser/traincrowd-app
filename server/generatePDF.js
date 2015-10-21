e67df7f794d79051c4e52534975a37ed272153af
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-10-21 16:45:20 +0200

    extends bill template

--- server/generatePDF.js
+++ server/generatePDF.js
@@ -1,34 +1,36 @@
 _worker = function() {
   var dataContext = {
     name: 'Thomas',
+    imgPath: path.join(__meteor_bootstrap__.serverDir, "../web.browser/app")
   };
-  console.log(dataContext);
+  // console.log(dataContext);
   var html = Spacebars.toHTML(dataContext, Assets.getText('bill.html'));
-  console.log(html);
-  // var webshot = Meteor.npmRequire('webshot');
-  // console.log(webshot);
+  // console.log(html);
+
   // var fs      = Npm.require('fs');
   var fileName = "/tmp/bill.pdf";
-  console.log(fileName);
   // Setup Webshot options
   var options = {
     "paperSize": {
       "format": "Letter",
-      "orientation": "portrait",
-      "margin": "1cm"
+      // "orientation": "portrait",
+      "margin": {
+        top: "2cm",
+        left: "1cm",
+        right: "1cm",
+        bottom: "1cm",
+      },
     },
     siteType: 'html',
     // phantomPath: '/usr/local/bin/phantomjs',
     // errorIfStatusIsNot200: true
   };
-  console.log(options);
   webshot(html, fileName, options, function(err) {
     console.log('inner');
     if (err) {
       return console.log(err);
     }
   });
-  console.log('after');
 
 };
 
