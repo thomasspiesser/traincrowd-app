372eb09b18e42af52c0368525003385620efe9f5
Thomas Spiesser <thomas.spiesser@gmail.com>
2015-11-13 23:51:40 +0100

    styles media-logos

--- client/application/home/home.js
+++ client/application/home/home.js
@@ -13,10 +13,6 @@ Template.home.onRendered(function() {
     classToAdd: 'visible animated fadeInLeft',
     classToRemove: 'invisible'
   });
-  $('.wow2').addClass('invisible').viewportChecker({
-    classToAdd: 'visible animated fadeInUp',
-    classToRemove: 'invisible'
-  });
 });
 
 Template.home.events({
